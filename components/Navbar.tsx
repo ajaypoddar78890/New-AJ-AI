1b9fd3e459a70bc7341f6db4acc22cf8757756c5
Your GitHub Username <ajay.poddar15fea@gmail.com>
2024-10-07 00:34:21 +0530

    adding the mobile sidebar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,11 @@
-import { Menu } from "lucide-react";
-import { Button } from "./ui/button";
+import MobileSidebar from "./mobile-sidebar";
 import { UserButton } from "@clerk/nextjs";
 import Link from "next/link";
 
 const Navbar = () => {
   return (
     <div className=" flex items-center p-4">
-      <Button variant={"ghost"} size={"icon"} className="md:hidden">
-        <Menu />
-      </Button>
+      <MobileSidebar />
 
       <div className="flex w-full justify-end">
         <Link href={"/"}>
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,10 +1,79 @@
+import {
+  Code2,
+  ImagesIcon,
+  LayoutDashboard,
+  MessagesSquare,
+  Music,
+  Settings,
+  VideotapeIcon,
+} from "lucide-react";
 import Link from "next/link";
+
+const routes = [
+  {
+    label: "Dashboard",
+    icon: LayoutDashboard,
+    href: "/dashbord",
+    color: "text-pink-400", // Icon color
+  },
+  {
+    label: "Conversation",
+    icon: MessagesSquare,
+    href: "/message",
+    color: "text-violet-500", // Icon color
+  },
+  {
+    label: "Image Generation",
+    icon: ImagesIcon,
+    href: "/image",
+    color: "text-yellow-500", // Icon color
+  },
+  {
+    label: "Video Generation",
+    icon: VideotapeIcon,
+    href: "/video",
+    color: "text-orange-500", // Icon color
+  },
+  {
+    label: "Music Generation",
+    icon: Music,
+    href: "/music",
+    color: "text-green-500", // Icon color
+  },
+  {
+    label: "Code  Generation",
+    icon: Code2,
+    href: "/code",
+    color: "text-gray-500", // Icon color
+  },
+  {
+    label: "Settings",
+    icon: Settings,
+    href: "/setting",
+    color: "text-red-500", // Icon color
+  },
+];
 
 const Sidebar = () => {
   return (
-    <div className="space-y-4 py-4  flex flex-col h-full bg-black  text-white">
+    <div className="space-y-4 py-4 flex flex-col h-full bg-black text-white">
       <div className="px-3 py-2 flex-1">
-        <Link href={"/dashbord"}>AJ -AI</Link>
+        <Link href={"/dashbord"}>AJ - AI</Link>
+        <div className="space-y-1">
+          {routes.map((route) => (
+            <Link
+              key={route.label}
+              href={route.href}
+              className="text-sm group flex p-3 w-full justify-start font-medium cursor-pointer"
+            >
+              <div className="flex items-center flex-1">
+                {/* Apply color only to the icon */}
+                <route.icon className={`h-5 w-5 mr-3 ${route.color}`} />
+                {route.label}
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
     </div>
   );
